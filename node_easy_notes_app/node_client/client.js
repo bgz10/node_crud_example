89360209068b0ee07c974d03e932525129edb924
_bogz_ <bgdmuntean@hotmail.com>
2019-10-13 20:17:46 +0200

    Updated to make post requests

--- node_easy_notes_app/node_client/client.js
+++ node_easy_notes_app/node_client/client.js
@@ -1,7 +1,21 @@
-var request = require('request');
+const request = require('request');
 request('http://localhost:3000/notes/5da2fedf17ab80cba93d43fa', function (error, response, body) {
     if (!error && response.statusCode == 200) {
-        var apiResponse = JSON.parse(body)
-        console.log(apiResponse.title) // Print the google web page.
+        var apiResponse = JSON.parse(body);
+        console.log(apiResponse.title); // Print the google web page.
      }
+});
+
+request.post('http://localhost:3000/notes', {
+  json: {
+    title: 'Buy the milk',
+    content: 'This note says to buy milk'
+  }
+}, (error, res, body) => {
+  if (error) {
+    console.error(error)
+    return
+  }
+  console.log(`statusCode: ${res.statusCode}`)
+  console.log(body)
 })
